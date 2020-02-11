5f8889e9d1def10a4600564db1fdc3eb93bb155e
nizar.boussebsi <nizar.bousebsi@gmail.com>
2020-02-11 08:32:43 +0000

    Add small test for get all account.

--- src/test/java/infrastructure/AccountRepositoryImplSmallTest.java
+++ src/test/java/infrastructure/AccountRepositoryImplSmallTest.java
@@ -1,5 +1,6 @@
 package infrastructure;
 
+import com.revolut.domain.Account;
 import com.revolut.rest.v1.controller.AccountControllerImpl;
 import com.revolut.rest.v1.service.AccountServiceImpl;
 import org.junit.jupiter.api.DisplayName;
@@ -9,6 +10,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -30,5 +36,30 @@ public class AccountRepositoryImplSmallTest {
 
         assertNotNull(resp);
         assertEquals(123L, resp);
+    }
+
+    @Test
+    @DisplayName("Verify get account is successful")
+    void itShouldGetAccountByIdSuccessfully() {
+        when(accountService.getAccountById("123")).thenReturn(Optional.of(new Account(123L, "Joe", new BigDecimal(100.00))));
+        var resp = accountController.getAccountById("123");
+
+        assertNotNull(resp);
+        assertEquals(Optional.of(new Account(123L, "Joe", new BigDecimal(100.00))), resp);
+    }
+
+    @Test
+    @DisplayName("Verify get all account is successful")
+    void itShouldGetAllAccountSuccessfully() {
+        List<Account> accountList = new ArrayList<>();
+        accountList.add(new Account(123L, "Joe", new BigDecimal(100.00)));
+        accountList.add(new Account(123L, "Mark", new BigDecimal(1500.00)));
+        accountList.add(new Account(123L, "Sarah", new BigDecimal(600.00)));
+        when(accountService.getAllAccount()).thenReturn(accountList);
+
+        var resp = accountController.getAllAccount();
+
+        assertNotNull(resp);
+        assertEquals(accountList, resp);
     }
 }
